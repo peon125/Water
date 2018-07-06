ce94dfd7a70d6faa8018ec6dbb9702ca7cee3a83
Arkadiusz Slowikowski <arkadiusz.slowikowski1996@gmail.com>
2018-07-06 08:05:40 +0200

    init

---
+++ Assets/FakeTouch.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FakeTouch : MonoBehaviour
+{
+    public static FakeTouch ciop;
+
+    public Touch fakeTouch;
+    public bool gotNewTouch;
+
+    Vector3 lastMousePosition;
+
+    private void Awake()
+    {
+        ciop = this;
+    }
+
+    void Update()
+    {
+        gotNewTouch = Input.GetMouseButton(0);
+
+        if (gotNewTouch)
+        {
+            fakeTouch = new Touch();
+            fakeTouch.fingerId = 10;
+            fakeTouch.position = Input.mousePosition;
+            fakeTouch.deltaTime = Time.deltaTime;
+            fakeTouch.deltaPosition = Input.mousePosition - lastMousePosition;
+            fakeTouch.phase = (Input.GetMouseButtonDown(0) ? TouchPhase.Began :
+                                (fakeTouch.deltaPosition.sqrMagnitude > 1f ? TouchPhase.Moved : TouchPhase.Stationary));
+            fakeTouch.tapCount = 1;
+        }
+
+        lastMousePosition = Input.mousePosition;
+    }
+}
---
+++ Assets/scripts/Cup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+
+public class Cup : MonoBehaviour
+{
+    [SerializeField]
+    Rigidbody rb;
+    [SerializeField]
+    float rotating_sensitivity;
+    [SerializeField]
+    float clampValue;
+
+    Touch[] touches;
+
+    private void Update()
+    {
+        AddFakeTouch();
+
+        TouchInput();
+    }
+
+    private void AddFakeTouch()
+    {
+        touches = new Touch[Input.touchCount + (FakeTouch.ciop.gotNewTouch ? 1 : 0)];
+
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            touches[i] = Input.GetTouch(i);
+        }
+
+        if (FakeTouch.ciop.gotNewTouch)
+        {
+            touches[touches.Length - 1] = FakeTouch.ciop.fakeTouch;
+        }
+    }
+
+    private void TouchInput()
+    {
+        if (touches.Length > 0)
+        {
+            float delta = Mathf.Clamp(touches[0].deltaPosition.x, -clampValue, clampValue);
+
+            rb.transform.eulerAngles += delta * Vector3.forward * rotating_sensitivity;
+        }
+    }
+}
